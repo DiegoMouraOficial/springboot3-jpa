5a09ddd212d96870c3b41dfc0f188ea678385e27
Diego Moura <programmercodeoficial@gmail.com>
2024-02-09 14:48:36 -0300

    criando associacoes Many-to-many para product e category com joinTable

--- src/main/java/io/diegomoura/springboot/config/TestConfig.java
+++ src/main/java/io/diegomoura/springboot/config/TestConfig.java
@@ -36,12 +36,23 @@ public class TestConfig implements CommandLineRunner {
 
         Category category1 = new Category(null, "Eletrônicos");
         Category category2 = new Category(null, "Livros");
-        Category category3 = new Category(null, "Vestuário");
+        Category category3 = new Category(null, "Escritorio");
 
         Product produto1 = new Product(null, "Código Limpo","Este livro...", 99.00, "");
         Product produto2 = new Product(null, "Macbook Pro","Este Macbook pro...", 19999.00, "");
         Product produto3 = new Product(null, "PC Gamer","Este PC Gamer...", 3499.98, "");
         Product produto4 = new Product(null, "Cadeira para Escritório","Está cadeira...", 999.00, "");
+
+        categoryRepository.saveAll((Arrays.asList(category1, category2, category3)));
+        productRepository.saveAll(Arrays.asList(produto1,produto2,produto3,produto4));
+
+        //Associacao entre objetos
+        produto1.getCategories().add(category2);
+        produto2.getCategories().add(category1);
+        produto3.getCategories().add(category1);
+        produto4.getCategories().add(category3);
+
+        productRepository.saveAll(Arrays.asList(produto1,produto2,produto3,produto4));
 
         User user1 = new User(null, "Lupi", "lupi@bol.com.br", "11945875287", "xxx");
         User user2 = new User(null, "Rebeca", "rebeca@outlook.com", "11978562541", "xxx");
@@ -51,8 +62,6 @@ public class TestConfig implements CommandLineRunner {
         Order order2 = new Order(null, Instant.parse("2024-02-22T04:23:15Z"), OrderStatus.PAGO, user2 );
         Order order3 = new Order(null, Instant.parse("2024-05-30T15:20:27Z"), OrderStatus.CANCELADO, user3 );
 
-        categoryRepository.saveAll((Arrays.asList(category1, category2, category3)));
-        productRepository.saveAll(Arrays.asList(produto1,produto2,produto3,produto4));
         userRepository.saveAll(Arrays.asList(user1, user2, user3));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
     }
